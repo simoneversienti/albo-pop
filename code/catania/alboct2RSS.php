3046851389137e1015d02f8ac834ea2be76d90b2
Cristiano Longo <cristianolongo@gmail.com>
2016-03-05 22:53:58 +0100

    spostare la descrizione nel titolo per albo POP ct #100

--- code/catania/alboct2RSS.php
+++ code/catania/alboct2RSS.php
@@ -27,7 +27,7 @@ $parser = new AlboComuneCTParser(2016);
 $feed=new RSSFeedGenerator("Albo del Comune di Catania", "Versione POP dell'Albo Pretorio del Comune di Catania", 
 		"http://www.comune.catania.gov.it/EtnaInWeb/AlboPretorio.nsf/HomePage?Open&buffer=A20110301121017437GH","http://dev.opendatasicilia.it/albopopct/alboct2RSS.php");
 foreach($parser as $r){
-	$feed->addItem($r->repertorio." - ".$r->tipo, $r->mittente_descrizione, null, $r->link, $r->link);
+	$feed->addItem($r->repertorio." - ".$r->tipo.": ".$r->mittente_descrizione, $r->mittente_descrizione, null, $r->link, $r->link);
 }
 
 //output
