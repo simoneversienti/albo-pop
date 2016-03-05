6e7f5592ad10609c235ff8d6f59c75f23e33d278
Cristiano Longo <cristianolongo@gmail.com>
2016-03-05 23:54:54 +0100

    Eliminare l'indicazione dell'ufficio dal titolo nel caso di comunicazione interna dal comune di Catania #104

--- code/catania/alboct2RSS.php
+++ code/catania/alboct2RSS.php
@@ -27,7 +27,9 @@ $parser = new AlboComuneCTParser(2016);
 $feed=new RSSFeedGenerator("Albo del Comune di Catania", "Versione POP dell'Albo Pretorio del Comune di Catania", 
 		"http://www.comune.catania.gov.it/EtnaInWeb/AlboPretorio.nsf/HomePage?Open&buffer=A20110301121017437GH","http://dev.opendatasicilia.it/albopopct/alboct2RSS.php");
 foreach($parser as $r){
-	$feed->addItem($r->repertorio." - ".$r->tipo.": ".$r->mittente_descrizione, $r->mittente_descrizione, null, $r->link, $r->link);
+	//remove the sender if it is internal to the municipality of Catania
+	$title=preg_replace('%^.*- COMUNE DI CATANIA *%', '',$r->mittente_descrizione,1);
+	$feed->addItem($title, $r->repertorio." - ".$r->tipo.": ".$r->mittente_descrizione, null, $r->link, $r->link);
 }
 
 //output
