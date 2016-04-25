7e920d7cf4195d2e69c389a52e32579ea0d01e69
Cristiano Longo <cristianolongo@gmail.com>
2016-04-25 11:23:19 +0200

    [Belpasso] : Scaricare il CSV dell'Albo pretorio di belpasso #140

---
+++ code/belpasso/albofeed.php
@@ -0,0 +1,46 @@
+<?php 
+/**
+ * This script produce the rss feed from the jCityGov albo of the municipality of
+ * Belpasso.
+ * 
+ * Copyright 2016 Cristiano Longo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require("../jCityGov/AlbojCityGovParser.php");
+require("../RSS/RSSFeedGenerator.php");
+
+$parser = new AlbojCityGovParser("http://belpasso.trasparenza-valutazione-merito.it/web/trasparenza/albo-pretorio;jsessionid=21B9E97A6704C607C6E169236A484C5F?p_p_id=jcitygovalbopubblicazioni_WAR_jcitygovalbiportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=exportList&_jcitygovalbopubblicazioni_WAR_jcitygovalbiportlet_format=csv");
+// $parser = AlboComuneCTParser::createByDate();
+// $feed=new RSSFeedGenerator("Albo del Comune di Catania", "Versione POP dell'Albo Pretorio del Comune di Catania", 
+// 		"http://www.comune.catania.gov.it/EtnaInWeb/AlboPretorio.nsf/HomePage?Open&buffer=A20110301121017437GH","http://dev.opendatasicilia.it/albopop/catania/alboct2RSS.php");
+// foreach($parser as $r){
+// 	//remove the sender if it is internal to the municipality of Catania
+// 	$title=preg_replace('%^.*- COMUNE DI CATANIA *%', '',$r->mittente_descrizione,1);
+// 	if (empty($title))
+// 		$feed->addItem('ERROR', null, null, $r->link, $r->link);
+// 	else
+// 		$feed->addItem($title, $r->repertorio." - ".$r->tipo.": ".$r->mittente_descrizione, null, $r->link, $r->link);
+// }
+
+// //output
+// header('Content-type: application/rss+xml; charset=UTF-8');
+// /*
+//  * Impostazioni locali in italiano, utilizzato per la stampa di data e ora
+//  * (il server deve avere il locale italiano installato
+//  */
+// setlocale(LC_TIME, 'it_IT');
+// echo $feed->getFeed();
+?>
---
+++ code/jCityGov/AlbojCityGovParser.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This class allows one to get and parse the entries of every jCityGov Albo Pretorio
+ * 
+ * Copyright 2016 Cristiano Longo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Cristiano Longo
+ */
+
+/**
+ * Convenience class to store albo entries
+ * 
+ * @author Cristiano Longo
+ */
+class AlbojCityGovEntry{
+	
+}
+
+/**
+ * Download and parse a jCityGov albo provided as csv.
+ */
+class  AlbojCityGovParser{
+	private $rows;
+	private $index;
+	
+	/**
+	 * Download a notice board provided as csv
+	 *
+	 * @param alboURI the url where it is possible to get the albo as csv
+	 * @throws Exception
+	 */
+	public function __construct($alboURI){
+		$h=curl_init($alboURI);
+		if (!$h) throw new Exception("Unable to initialize cURL session");
+		curl_setopt($h, CURLOPT_RETURNTRANSFER, TRUE);
+		$retrievedData=curl_exec($h);
+		if($retrievedData==FALSE)
+			throw new Exception("Unable to execute request: ".curl_error($h));
+		curl_close($h);
+		$this->rows=str_getcsv($retrievedData,"\n");
+		$this->numItems=count($this->rows)-1;
+		$this->index=0;
+		echo $retrievedData;
+	}	
+}
