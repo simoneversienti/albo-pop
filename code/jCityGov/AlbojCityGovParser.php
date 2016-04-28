74ac6bbf0067766143139fc8c783ad0dc1e8128a
Cristiano Longo <cristianolongo@gmail.com>
2016-04-28 23:07:34 +0200

    [belpasso] parsing del csv #143 and [belpasso] realizzare feed rss #142

--- code/jCityGov/AlbojCityGovParser.php
+++ code/jCityGov/AlbojCityGovParser.php
@@ -20,19 +20,85 @@
  * @author Cristiano Longo
  */
 
+define('DATE_FORMAT','d/m/Y');
+
 /**
  * Convenience class to store albo entries
  * 
  * @author Cristiano Longo
  */
 class AlbojCityGovEntry{
+	public $proponente;
+	public $proponente_descrizione;
+	public $oggetto;
+	public $anno;
+	public $numero;
+	public $anno_registrazione;
+	public $numero_registrazione;
+	public $anno_protocollo;
+	public $numero_protocollo;
+	public $classifica_descrizione;
+	public $dirigente_descrizione;
+	public $assessore_descrizione;
+	public $data_esecutivita;
+	public $mittente;
+	public $numero_allegati;
+	public $data_documento;
+	public $data_atto;
+	public $spesa_prevista;
+	public $titolo_categoria;
+	public $titolo_sottocategoria;
+	public $contenuto;
+	public $estremi_dei_principali_documenti;
+	public $data_inizio_pubblicazione;
+	public $data_fine_pubblicazione;
 	
+	/**
+	 * Create an entry from a csv row
+	 */
+	public function __construct($rowStr){
+		$row=str_getcsv($rowStr);
+		$this->proponente=$row[0];
+		$this->proponente_descrizione=$row[1];
+		$this->oggetto=$row[2];
+		$this->anno=$row[3];
+		$this->numero=$row[4];
+		$this->anno_registrazione=$row[5];
+		$this->numero_registrazione=$row[6];
+		$this->anno_protocollo=$row[7];
+		$this->numero_protocollo=$row[8];
+		$this->classifica_descrizione=$row[9];
+		$this->dirigente_descrizione=$row[10];
+		$this->assessore_descrizione=$row[11];
+		$this->data_esecutivita=AlbojCityGovEntry::getDate($row[12]);
+		$this->mittente=$row[13];
+		$this->numero_allegati=$row[14];
+		$this->data_documento=AlbojCityGovEntry::getDate($row[15]);
+		$this->data_atto=AlbojCityGovEntry::getDate($row[16]);
+		$this->spesa_prevista=$row[17];
+		$this->titolo_categoria=$row[18];
+		$this->titolo_sottocategoria=$row[19];
+		$this->contenuto=$row[20];
+		$this->estremi_dei_principali_documenti=$row[21];
+		$this->data_inizio_pubblicazione=AlbojCityGovEntry::getDate($row[22]);
+		$this->data_fine_pubblicazione=AlbojCityGovEntry::getDate($row[23]);
+	}	
+	
+	/**
+	 * Get a date from a string, if the string is not null and not empty.
+	 * Otherwise, return null.
+	 */
+	private static function getDate($dateStr){
+		if ($dateStr==null || strlen($dateStr)==0)
+			return null;
+		return DateTime::createFromFormat(DATE_FORMAT, $dateStr, new DateTimeZone('Europe/Rome'));
+	}
 }
 
 /**
  * Download and parse a jCityGov albo provided as csv.
  */
-class  AlbojCityGovParser{
+class  AlbojCityGovParser implements Iterator{
 	private $rows;
 	private $index;
 	
@@ -51,8 +117,32 @@ class  AlbojCityGovParser{
 			throw new Exception("Unable to execute request: ".curl_error($h));
 		curl_close($h);
 		$this->rows=str_getcsv($retrievedData,"\n");
-		$this->numItems=count($this->rows)-1;
-		$this->index=0;
+		$this->index=1;
 		echo $retrievedData;
 	}	
+	
+	//Iterator functions,  see http://php.net/manual/en/class.iterator.php
+	
+	public function current(){
+		echo "Index ".$this->index;
+		return new AlbojCityGovEntry($this->rows[$this->index]);
+	}
+	
+	
+	public function key (){
+		return $this->index;
+	}
+	
+	public function next(){
+		++$this->index;
+	}
+	
+	public function rewind(){
+		$this->index=1;
+	}
+	
+	public function valid(){
+		$numItems=count($this->rows)-1;
+		return $numItems>1 && $this->index<($numItems+1);
+	}
 }
